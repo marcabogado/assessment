d4e8755d6ec2bfbb3955ab60e15efea74b7ada04
marcabogado <58835044+marcabogado@users.noreply.github.com>
2021-09-28 10:13:08 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ function App() {
   const [users, setUsers] = useState([]);
   
   const addUserHandler = (userd) =>{
-    console.log(userd);
+    //console.log(userd);
     setUsers([...users, {id: uuid(), ...userd }]);
   };
 
